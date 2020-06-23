ac1dc63f6a4372a413f0d1a412ddd8a03bfef956
Valérie <gagnon@MacBook-Pro-de-Valerie.local>
2020-06-23 14:46:38 -0400

    Afficher produit categorie forfait

--- theme/partials/loop.php
+++ theme/partials/loop.php
@@ -43,11 +43,15 @@
 	<?php
 		$args = array(
 			'post_type' => 'product',
+			'category_name' => 'nos-forfaits' ,
 			'posts_per_page' => 12
 			);
+	
 		$loop = new WP_Query( $args );
 		if ( $loop->have_posts() ) {
+			
 			while ( $loop->have_posts() ) : $loop->the_post();
+			
 				wc_get_template_part( 'content', 'product' );
 			
 			endwhile;
