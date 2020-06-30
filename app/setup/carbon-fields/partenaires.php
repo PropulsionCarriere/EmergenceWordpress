7dd0b2bf7d5e19e11ecfeefad9517e25fa5d22c6
Valérie <gagnon@MacBook-Pro-de-Valerie.local>
2020-06-30 13:21:58 -0400

    Tentative d'ajouter un champs I guest?

---
+++ app/setup/carbon-fields/partenaires.php
@@ -0,0 +1,9 @@
+<?php use Carbon_Fields\Container;
+use Carbon_Fields\Field;
+
+Container::make( 'user_meta', 'Address' )
+    ->add_fields( array(
+        Field::make( 'text', 'crb_name', 'Nom de lorganisme' ),
+        Field::make( 'image', 'crb_image', 'Logo de organisme' ),
+    ) );
+?>
