4043a0980ec5d3023129b3281659ca7c3a4a5d52
Valérie <gagnon@MacBook-Pro-de-Valerie.local>
2020-06-30 10:26:38 -0400

    J'ai enlever la fonction pour les articles disposer en colonne qui ne marchait pas.

--- theme/functions.php
+++ theme/functions.php
@@ -121,6 +121,8 @@ add_image_size( "post_image", 2000, 400, ['center', 'center'] );
 add_image_size( "big_thumbnail", 200, 200, true);
 add_image_size("mainheader",1024,600);
 add_image_size("secondheader",1024,400);
+
+
 add_action( 'widgets_init', function(){
 	register_sidebar([
 		'name' 					=> 'footer-left',
