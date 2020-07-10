abe837ccfcf217d2d188869f70a0caccdce03f5c
Valérie <gagnon@MacBook-Pro-de-Valerie.local>
2020-07-09 22:20:59 -0400

    tentative de bouton panier

--- theme/functions.php
+++ theme/functions.php
@@ -151,4 +151,16 @@ add_action( 'widgets_init', function(){
 		'before_widget'	=> '<div class="widget flex-fill mt-5">',
 		'after_widget'	=> '</div>',
 	]);
-} );
+} );add_filter( 'woocommerce_add_to_cart_fragments', 'woocommerce_header_add_to_cart_fragment' );
+
+function woocommerce_header_add_to_cart_fragment( $fragments ) {
+	global $woocommerce;
+
+	ob_start();
+
+	?>
+	<a class="cart-customlocation" href="<?php echo esc_url(wc_get_cart_url()); ?>" title="<?php _e('View your shopping cart', 'woothemes'); ?>"><?php echo sprintf(_n('%d item', '%d items', $woocommerce->cart->cart_contents_count, 'woothemes'), $woocommerce->cart->cart_contents_count);?> - <?php echo $woocommerce->cart->get_cart_total(); ?></a>
+	<?php
+	$fragments['a.cart-customlocation'] = ob_get_clean();
+	return $fragments;
+}
